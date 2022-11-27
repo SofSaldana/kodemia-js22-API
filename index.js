f43b8495733ae73b21066717501bdcc8830c1d0d
Sof <sofie151091@gmail.com>
2022-11-27 13:41:39 -0600

    tarea finalizada

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get("/hi",(req, res)=>{
     res.send("Hello Chofis ♥");
 });
 
-app.listen(8000, ()=>{ 
- if(!err)console.log("Escuchando peticiones HTTP en el puerto 8000"); 
+app.listen(8000, (err)=>{ 
+ if (!err) console.log("Escuchando peticiones HTTP en el puerto 8000"); 
  else console.error ("Ups, an error has ocurred",err);
 });
